fb6e3e143fbdccebe461c7489a4c040d1d3d99b3
doramart <doramart@qq.com>
2019-10-26 00:38:09 +0800

    update egg-dora-rendercms

---
+++ app/assets/vue-admin-demo/src/views/common/TopBar.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="dr-toolbar">
+    <div class="option-button el-col-6">
+      <!-- TOPBARLEFT -->
+    </div>
+    <div class="el-col-18">
+      <!-- TOPBARRIGHT -->
+    </div>
+  </div>
+</template>
+<script>
+import { bakUpCMSData } from "@/api/helpCenter";
+export default {
+  props: {
+    device: String,
+    pageInfo: Object,
+    type: String,
+    ids: Array
+  },
+  data() {
+    return {
+      selectUserList: [],
+      searchkey: ""
+    };
+  },
+  methods: {
+    // TOPBARLEFTOPTION
+    // TOPBARRIGHTSEARCH
+  },
+  components: {}
+};
+</script>
+<style lang="scss">
+</style>
